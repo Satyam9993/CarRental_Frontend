f63f9e9fd6aeff3c5efb9d836a9405ba365de286
A-Celsius <akshuchauhan301@gmail.com>
2023-03-14 23:25:55 +0530

    first

--- src/App.js
+++ src/App.js
@@ -26,7 +26,8 @@ const router = createBrowserRouter([
 
 const App = () => {
   return (
-    <RouterProvider router={router} />
+    <RouterProvider router={router} />,
+    <p>Hello Akshay</p>
   )
 }
 
