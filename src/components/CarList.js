942efed97f52b768fa3b5c0949fa4d93753790f4
Satyam Katiyar <satyamkatiyar9993@gmail.com>
2023-04-27 13:52:06 +0530

    change car cart

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,86 +1,72 @@
 import React from 'react'
+import CarCard from './CarCard'
 
 const CarList = () => {
-  const products = [
-    {
-        id: 1,
-        name: 'Basic Tee',
-        href: '#',
-        imageSrc: 'https://img.freepik.com/free-photo/black-mini-coupe-road_114579-5056.jpg?size=626&ext=jpg&ga=GA1.1.90537765.1677043202&semt=robertav1_2_sidr',
-        imageAlt: "Front of men's Basic Tee in black.",
-        price: '$35',
-        color: 'Black',
-    },
-    {
-        id: 2,
-        name: 'Basic Tee',
-        href: '#',
-        imageSrc: 'https://img.freepik.com/free-photo/black-mini-coupe-road_114579-5056.jpg?size=626&ext=jpg&ga=GA1.1.90537765.1677043202&semt=robertav1_2_sidr',
-        imageAlt: "Front of men's Basic Tee in black.",
-        price: '$35',
-        color: 'Black',
-    },
-    {
-        id: 3,
-        name: 'Basic Tee',
-        href: '#',
-        imageSrc: 'https://img.freepik.com/free-photo/black-mini-coupe-road_114579-5056.jpg?size=626&ext=jpg&ga=GA1.1.90537765.1677043202&semt=robertav1_2_sidr',
-        imageAlt: "Front of men's Basic Tee in black.",
-        price: '$35',
-        color: 'Black',
-    },
-    {
-        id: 4,
-        name: 'Basic Tee',
-        href: '#',
-        imageSrc: 'https://img.freepik.com/free-photo/black-mini-coupe-road_114579-5056.jpg?size=626&ext=jpg&ga=GA1.1.90537765.1677043202&semt=robertav1_2_sidr',
-        imageAlt: "Front of men's Basic Tee in black.",
-        price: '$35',
-        color: 'Black',
-    },
-    {
-        id: 5,
-        name: 'Basic Tee',
-        href: '#',
-        imageSrc: 'https://img.freepik.com/free-photo/black-mini-coupe-road_114579-5056.jpg?size=626&ext=jpg&ga=GA1.1.90537765.1677043202&semt=robertav1_2_sidr',
-        imageAlt: "Front of men's Basic Tee in black.",
-        price: '$35',
-        color: 'Black',
-    },
-]
-  return (
-    <div className="bg-white">
+    const products = [
+        {
+            id: 1,
+            name: 'Puma Shoes',
+            desc : "The best shoes in the marketplace",
+            href: '#',
+            imageSrc: "https://unsplash.com/photos/IJjfPInzmdk/download?force=true&w=1920",
+            imageAlt: "Puma Shoes",
+            price: '$2/hr',
+            color: 'Black',
+        },
+        {
+            id: 2,
+            name: 'Basic Tee',
+            desc : "The best shoes in the marketplace",
+            href: '#',
+            imageSrc: "https://unsplash.com/photos/IJjfPInzmdk/download?force=true&w=1920",
+            imageAlt: "Front of men's Basic Tee in black.",
+            price: '$35',
+            color: 'Black',
+        },
+        {
+            id: 3,
+            name: 'Basic Tee',
+            desc : "The best shoes in the marketplace",
+            href: '#',
+            imageSrc: "https://unsplash.com/photos/IJjfPInzmdk/download?force=true&w=1920",
+            imageAlt: "Front of men's Basic Tee in black.",
+            price: '$35',
+            color: 'Black',
+        },
+        {
+            id: 4,
+            name: 'Basic Tee',
+            desc : "The best shoes in the marketplace",
+            href: '#',
+            imageSrc: "https://unsplash.com/photos/IJjfPInzmdk/download?force=true&w=1920",
+            imageAlt: "Front of men's Basic Tee in black.",
+            price: '$35',
+            color: 'Black',
+        },
+        {
+            id: 5,
+            name: 'Basic Tee',
+            desc : "The best shoes in the marketplace",
+            href: '#',
+            imageSrc: "https://unsplash.com/photos/IJjfPInzmdk/download?force=true&w=1920",
+            imageAlt: "Front of men's Basic Tee in black.",
+            price: '$35',
+            color: 'Black',
+        },
+    ]
+    return (
+        <div className="bg-white">
             <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
                 <h2 className="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
 
                 <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                     {products.map((product) => (
-                        <div key={product.id} className="group relative">
-                            <div className="min-h-80 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                                <img
-                                    src={product.imageSrc}
-                                    alt={product.imageAlt}
-                                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                                />
-                            </div>
-                            <div className="mt-4 flex justify-between">
-                                <div>
-                                    <h3 className="text-sm text-gray-700">
-                                        <a href={product.href}>
-                                            <span aria-hidden="true" className="absolute inset-0" />
-                                            {product.name}
-                                        </a>
-                                    </h3>
-                                    <p className="mt-1 text-sm text-gray-500">{product.color}</p>
-                                </div>
-                                <p className="text-sm font-medium text-gray-900">{product.price}</p>
-                            </div>
-                        </div>
+                        <CarCard key={product.id} product={product} />
                     ))}
                 </div>
             </div>
         </div>
-  )
+    )
 }
 
 export default CarList
