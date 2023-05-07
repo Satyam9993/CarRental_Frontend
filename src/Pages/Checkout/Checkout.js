00c592421205aae96a0a246bf3f43396c9a2dc38
Satyam Katiyar <satyamkatiyar9993@gmail.com>
2023-05-07 12:45:59 +0530

    new commit

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import Checkoutleft from '../../components/Checkoutleft';
 import CheckOutRight from '../../components/CheckOutRight';
+import { Link } from 'react-router-dom';
 const Checkout = () => {
     return (
         <>
             <div className="min-w-screen min-h-screen bg-gray-50 py-5">
                 <div className="px-5">
                     <div className="mb-2">
-                        <a href="#" className="focus:outline-none hover:underline text-gray-500 text-sm"><i className="mdi mdi-arrow-left text-gray-400"></i>Back</a>
+                        <Link to="/product" className="focus:outline-none hover:underline text-gray-500 text-sm"><i className="mdi mdi-arrow-left text-gray-400"></i>Back</Link>
                     </div>
                     <div className="mb-2">
                         <h1 className="text-3xl md:text-5xl font-bold text-gray-600">Checkout.</h1>
                     </div>
                     <div className="mb-5 text-gray-400">
-                        <a href="#" className="focus:outline-none hover:underline text-gray-500">Home</a> / <a href="#" className="focus:outline-none hover:underline text-gray-500">Cart</a> / <span className="text-gray-600">Checkout</span>
+                        <Link to="/" className="focus:outline-none hover:underline text-gray-500">Home</Link> / <Link to="/product" className="focus:outline-none hover:underline text-gray-500">Product</Link> / <span className="text-gray-600">Checkout</span>
                     </div>
                 </div>
                 <div className="w-full bg-white border-t border-b border-gray-200 px-5 py-10 text-gray-800">
