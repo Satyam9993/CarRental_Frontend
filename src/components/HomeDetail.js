6643af7363981751cbb638a5fa51df26c5b2913b
Satyam Katiyar <satyamkatiyar9993@gmail.com>
2023-05-29 11:50:34 +0530

    vivek commiy

--- src/components/HomeDetail.js
+++ src/components/HomeDetail.js
@@ -7,17 +7,17 @@ const HomeDetail = () => {
         {
             name: 'Demand Vehicle from anywhere!',
             description:
-                'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+                'Imagine a world where transportation is effortless and convenient, where you can summon a vehicle to your exact location with just a few taps on your smartphone. With the "Demand Vehicle from anywhere" service, this vision becomes a reality.',
             icon: CloudArrowUpIcon,
         },
         {
             name: 'Secure and Tested Vehicle',
-            description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+            description: 'It refers to a vehicle that has undergone rigorous testing and meets high standards of safety and security. It implies that the vehicle has been thoroughly evaluated, inspected, and certified to ensure its reliability, durability, and compliance with relevant regulations.',
             icon: LockClosedIcon,
         },
         {
             name: 'Minimun Price and hight enjoyment',
-            description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+            description: 'Introducing an incredible opportunity to maximize your enjoyment without stretching your budget – the perfect blend of minimum price and high enjoyment. Imagine being able to indulge in exciting experiences and entertainment without worrying about exorbitant costs..',
             icon: BanknotesIcon,
         },
     ]
@@ -30,8 +30,7 @@ const HomeDetail = () => {
                             <h2 className="text-base font-semibold leading-7 text-[#fa5c43]">Drive Secure, Safe and At Your prefference</h2>
                             <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better Drive, Makes you happy!</p>
                             <p className="mt-6 text-lg leading-8 text-gray-600">
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
-                                iste dolor cupiditate blanditiis ratione.
+                            Discover the transformative power of a better drive that brings happiness to your journey. Picture yourself behind the wheel, feeling a sense of exhilaration and contentment as you navigate the open road.
                             </p>
                             <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
                                 {features.map((feature) => (
